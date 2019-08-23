adcce86fc6d9064b9c931a660c6e797e1a746958
mateusz_kalwaj <tynozobacz@gmail.com>
2019-08-24 01:22:17 +0200

    Map Based services implemented

---
+++ pet-clinic-data/src/main/java/mk/pets/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package mk.pets.petclinic.services.map;
+
+import mk.pets.petclinic.model.Vet;
+import mk.pets.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
