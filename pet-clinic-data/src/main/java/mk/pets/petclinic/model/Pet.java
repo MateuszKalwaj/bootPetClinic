de0021addfadcccfdd92cfa8f17deba803741abd
mateusz_kalwaj <tynozobacz@gmail.com>
2019-08-24 00:15:45 +0200

    BaseEntity class added

--- pet-clinic-data/src/main/java/mk/pets/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/mk/pets/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package mk.pets.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
--- pet-clinic-data/src/main/java/mk/pets/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/mk/pets/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package mk.pets.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
