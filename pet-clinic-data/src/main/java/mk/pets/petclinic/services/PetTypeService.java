ce2ab79a4428106085907b778e945f60216e64d7
mateusz_kalwaj <tynozobacz@gmail.com>
2019-08-27 22:59:35 +0200

    added: PetTypeService with Map class

---
+++ pet-clinic-data/src/main/java/mk/pets/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package mk.pets.petclinic.services;
+
+import mk.pets.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/mk/pets/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package mk.pets.petclinic.services.map;
+
+import mk.pets.petclinic.model.PetType;
+import mk.pets.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
