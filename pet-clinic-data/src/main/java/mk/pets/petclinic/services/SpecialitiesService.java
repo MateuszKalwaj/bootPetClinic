d4e7d651d549a0882ce2e0cf51ed558c849ff79b
mateusz_kalwaj <tynozobacz@gmail.com>
2019-08-28 23:56:27 +0200

    specialties map with interface added

---
+++ pet-clinic-data/src/main/java/mk/pets/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package mk.pets.petclinic.services;
+
+import mk.pets.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/mk/pets/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package mk.pets.petclinic.services.map;
+
+import mk.pets.petclinic.model.Speciality;
+import mk.pets.petclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
