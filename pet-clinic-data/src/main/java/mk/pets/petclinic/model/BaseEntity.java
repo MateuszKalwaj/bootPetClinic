b8be4a3dadc6f78ff868634037d28771c49b3b17
mateusz_kalwaj <tynozobacz@gmail.com>
2019-08-30 12:20:24 +0200

    added JPA entities annotations

--- pet-clinic-data/src/main/java/mk/pets/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/mk/pets/petclinic/model/BaseEntity.java
@@ -1,9 +1,16 @@
 package mk.pets.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass //indicates the class that we are going to inherit from
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //that means that provider must assign primary keys
     private Long id;
 
     public Long getId() {
