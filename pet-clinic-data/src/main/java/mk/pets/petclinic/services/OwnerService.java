cc3f9949b8b08f334fb45c4552c5352fe64afb83
mateusz_kalwaj <tynozobacz@gmail.com>
2019-09-08 22:15:09 +0200

    ownerController tested, added search bar to owners

--- pet-clinic-data/src/main/java/mk/pets/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/mk/pets/petclinic/services/OwnerService.java
@@ -2,9 +2,14 @@ package mk.pets.petclinic.services;
 
 import mk.pets.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner>findAllByLastNameLike(String lastName);
+
     /* not needed after extending interface
     Owner findById(Long id);
     Owner save(Owner owner);
