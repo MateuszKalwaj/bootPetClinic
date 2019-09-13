3389800848e9a40a62367ddfc82eff707ddc6b16
mateusz_kalwaj <tynozobacz@gmail.com>
2019-09-13 16:30:19 +0200

    visit date issue fixed. added tests for Pet Map Service

--- pet-clinic-data/src/main/java/mk/pets/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/mk/pets/petclinic/model/Visit.java
@@ -1,6 +1,7 @@
 package mk.pets.petclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -15,6 +16,7 @@ import java.time.LocalDate;
 public class Visit extends BaseEntity {
 
     @Column(name = "date")
+    @DateTimeFormat(pattern = "yyy-MM-dd")
     private LocalDate date;
 
     @Column(name = "description")
